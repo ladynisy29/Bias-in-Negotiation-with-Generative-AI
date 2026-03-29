5139ac2a0435d90116f02d1bcf646a0fd28c7ee6
tanyaa1509 <tetanastadnik219@gmail.com>
2026-03-29 21:26:03 +0300

    add user profile model

--- core/settings.py
+++ core/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ai_negotiation_db',
+        'USER': 'postgres',
+        'PASSWORD': 'ainegotiation',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
