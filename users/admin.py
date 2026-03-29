40f9636ae939c0c44b7712fe19ba0818286b549a
tanyaa1509 <tetanastadnik219@gmail.com>
2026-03-29 22:00:19 +0300

    add negotiation session model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import UserProfile, NegotiationSession
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(NegotiationSession)
--- users/models.py
+++ users/models.py
@@ -14,3 +14,24 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
+class NegotiationSession(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # basic info
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    # negotiation data
+    scenario = models.TextField()
+    strategy = models.CharField(max_length=100)
+
+    # results
+    outcome = models.TextField(blank=True, null=True)
+    score = models.IntegerField(default=0)
+
+    # timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
