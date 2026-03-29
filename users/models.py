2b016d830d4444da230eb3f829ed31ce2c70d551
tanyaa1509 <tetanastadnik219@gmail.com>
2026-03-29 21:44:46 +0300

    add user profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # demographics
+    age = models.IntegerField(null=True, blank=True)
+    gender = models.CharField(max_length=20, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    education = models.CharField(max_length=100, null=True, blank=True)
+    occupation = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
